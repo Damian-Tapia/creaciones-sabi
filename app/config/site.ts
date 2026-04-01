a7af0cb66636db2dc75e033af29eae7f61effff8
Damian Tapia <damian.tapia45@gmail.com>
2026-03-31 20:45:54 -0600

    Changed default message

--- app/config/site.ts
+++ app/config/site.ts
@@ -1,6 +1,6 @@
 const WHATSAPP_PHONE = process.env.NEXT_PUBLIC_WHATSAPP_PHONE ?? "5210000000000";
 const WHATSAPP_MESSAGE = encodeURIComponent(
-  "Hola Creaciones Sabi, quiero cotizar mi evento"
+  "Hola Creaciones Sabi!, quiero cotizar productos de tu tienda en linea"
 );
 
 export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_PHONE}?text=${WHATSAPP_MESSAGE}`;
